18b2b8b755eb6842bfc470130bd6e727bbadb0c2
amoya <amoya@enco>
2018-02-02 19:23:03 -0300

    config virtuoso updated

--- scripts/setup/configure.sh
+++ scripts/setup/configure.sh
@@ -1,10 +1,15 @@
 #virtuoso.ini
 # parar el servicio por pid
- kill `pidof <name>`
-# preguntar al usuario la memoria disponible para virtuoso ofresiendo sugerencias basadas en la memoria disponible
+ kill `pidof virtuoso-t`
+ 
+# preguntar al usuario la memoria disponible para virtuoso ofreciendo sugerencias basadas en la memoria disponible
 # editar el fichero virtuoso.ini
 # NumberOfBuffers
 # MaxDirtyBuffers
+system_memory_in_mb=`free -m | awk '/:/ {print $2;exit}'`
+numberOfBuffers=$(($system_memory_in_mb * 83.0078125))
+MaxDirtyBuffers=$(($system_memory_in_mb * 63.4765625))
+
 
 # preguntar al usuario el directorio de datos, por defecto el $home
 # editar el fichero virtuoso.ini
