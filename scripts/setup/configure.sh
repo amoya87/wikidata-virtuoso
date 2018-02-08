2dd6d4c4a1e7b5275a70cde9d4b6cc0dfd874595
amoya <amoya@enco>
2018-02-08 15:46:30 -0300

    virtuoso configure script finished and tested

--- scripts/setup/configure.sh
+++ scripts/setup/configure.sh
@@ -1,19 +1,41 @@
+#!/bin/bash
 #virtuoso.ini
-# parar el servicio por pid
- kill `pidof virtuoso-t`
- 
-# preguntar al usuario la memoria disponible para virtuoso ofreciendo sugerencias basadas en la memoria disponible
-# editar el fichero virtuoso.ini
-# NumberOfBuffers
-# MaxDirtyBuffers
+# stop service by pid
+kill `pidof virtuoso-t`
+
 system_memory_in_mb=`free -m | awk '/:/ {print $2;exit}'`
-numberOfBuffers=$(($system_memory_in_mb * 83.0078125))
-MaxDirtyBuffers=$(($system_memory_in_mb * 63.4765625))
+echo La memoria libre es: $system_memory_in_mb
+# each buffer caches a 8K page of data and occupies approx. 8700 bytes of memory
+# it's suggested to set this value to 65 % of ram for a db only server
+# so if you have 32 GB of ram: 32*1000^3*0.65/8700 = 2390804
+# default is 2000 which will use 16 MB ram ;)
+numberOfBuffers=$(($system_memory_in_mb * 80))
+read -p "Enter the number of buffers [$numberOfBuffers]: " numberOfBuffers;
+if [[ ( $numberOfBuffers = "") ]] ;then
+	numberOfBuffers=$(($system_memory_in_mb * 80));
+fi;
+echo NumberOfBuffers = $numberOfBuffers
+
+maxDirtyBuffers=$(($numberOfBuffers * 3/4))
+echo MaxDirtyBuffers = $maxDirtyBuffers
+
+# set this to 1/4th of NumberOfBuffers
+maxCheckpointRemap=$(($numberOfBuffers * 1/4))
+echo MaxCheckpointRemap = $maxCheckpointRemap
+
+sed -i "s/^NumberOfBuffers.*/NumberOfBuffers          = ${numberOfBuffers}/g" /usr/local/var/lib/virtuoso/db/virtuoso.ini
+sed -i "s/^MaxDirtyBuffers.*/MaxDirtyBuffers          = ${maxDirtyBuffers}/g" /usr/local/var/lib/virtuoso/db/virtuoso.ini
+sed -i "s/^MaxCheckpointRemap.*/MaxCheckpointRemap              = ${maxCheckpointRemap}/g" /usr/local/var/lib/virtuoso/db/virtuoso.ini
+
+# ask for path of input data
+read -p "Enter the path to dir of input data [/path/to/data]: " DIR;
+echo El DIR es : $DIR
 
 
-# preguntar al usuario el directorio de datos, por defecto el $home
-# editar el fichero virtuoso.ini
 # DirsAllowed  = ..., /path/to/data
+if [[ -d $DIR ]] ; then 
+	sed -i "/^DirsAllowed.*/s|$|, ${DIR}|g" /usr/local/var/lib/virtuoso/db/virtuoso.ini
+fi;
 
-# riniciar el servicio virtuoso
+# start virtuoso service
 virtuoso-t &
