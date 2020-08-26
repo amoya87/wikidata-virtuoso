72bbae7ee9ac276b80d893221abc3cbe4f0c2e94
aidhog <aidhog@gmail.com>
2020-08-26 16:22:50 -0400

    Update setup.sh
    Fixes SSH version issue: https://stackoverflow.com/questions/52969379/virtuoso-open-source-installation-problem

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -1,6 +1,6 @@
 wget https://github.com/openlink/virtuoso-opensource/releases/download/v7.2.5.1/virtuoso-opensource-7.2.5.tar.gz
 sudo apt-get -y install dpkg-dev build-essential
-sudo apt-get -y install autoconf automake libtool flex bison gperf gawk m4 make odbcinst libxml2-dev libssl-dev libreadline-dev
+sudo apt-get -y install autoconf automake libtool flex bison gperf gawk m4 make odbcinst libxml2-dev libssl1.0-dev libreadline-dev
 tar xvpfz virtuoso-opensource-7.2.5.tar.gz
 cd virtuoso-opensource-7.2.5/
 ./autogen.sh
@@ -11,3 +11,4 @@ sudo make install
 cd /usr/local/var/lib/virtuoso/db/
 sudo chown -R $(whoami) .
 virtuoso-t &
+sudo apt-get install libssl-dev
